4de675ff97bebfb5b149dda53b7501b50f8f8edb
Averell <DQDPDSZAAJCR@spammotel.com>
2009-08-12 16:30:18 +0200

    gemified rails plugin

--- lib/resource_controller_respond_to.rb
+++ lib/resource_controller_respond_to.rb
@@ -6,52 +6,49 @@ module ResourceControllerRespondTo
     base.extend ClassMethods
     base.class_eval do
       class << self        
-        puts '##loading...'
         alias_method_chain :init_default_actions, :json
         alias_method_chain :init_default_actions, :xml
       end
     end
   end
-
+  
   module ClassMethods
     def init_default_actions_with_json(klass)
       init_default_actions_without_json(klass)
-      klass.class_eval do    
-        puts '##loading json'
-        index.wants.json { render :json => collection.to_json, :status => :ok }
-        show.wants.json  { render :json  => object}
-        edit.wants.json  { render :json  => object}
-        new_action.wants.json { render :json => object }
-
-        create.wants.json { render :json => object, :status => :created, :location => object }
-        create.failure.wants.json { render :json => object.errors, :status => :unprocessable_entity }
-        destroy.wants.json { head :ok }
-        update.wants.json { head :ok }
-        update.failure.wants.json { render :json => object.errors, :status => :unprocessable_entity }
-        # Response doesn't make sense for edit action
+      klass.class_eval do
+        index.wants.json { render :json => collection, :status => :ok }
+        show.wants.json  { render :json => object }
+        
+        update.wants.json { head :accepted }
+        
+        create.wants.xml { redirect_to object,  :status => :created }
+        destroy.wants.xml { redirect_to collection, :status => :accepted }
+        
+        for action in [create, update]
+          action.failure.wants.json { render :json => object.errors, :status => :unprocessable_entity }
+        end
       end
     end
-
-
+    
+    
     def init_default_actions_with_xml(klass)
       init_default_actions_without_xml(klass)
       klass.class_eval do
-        puts '##loading xml'
-        index.wants.xml { render :xml => collection.to_xml, :status => :ok }
-        show.wants.xml  { render :xml  => object}
-        edit.wants.xml  { render :xml  => object}
-        new_action.wants.xml { render :xml => object }
-      
-        create.wants.xml { render :xml => object, :status => :created, :location => object }
-        create.failure.wants.xml { render :xml => object.errors, :status => :unprocessable_entity }
-        destroy.wants.xml { head :ok }
-        update.wants.xml { head :ok }
-        update.failure.wants.xml { render :xml => object.errors, :status => :unprocessable_entity }
-        # Response doesn't make sense for edit action
+        index.wants.xml { render :xml => collection, :status => :ok }
+        show.wants.xml  { render :xml => object }
+        
+        create.wants.xml { redirect_to object,  :status => :created }
+        destroy.wants.xml { redirect_to collection, :status => :accepted }
+        
+        update.wants.xml { head :accepted }
+        
+        for action in [create, update]
+          action.failure.wants.xml { render :xml => object.errors, :status => :unprocessable_entity }
+        end
       end
     end
-
-  end     
+    
+  end
 end
 
 ResourceController::Controller.send :include, ResourceControllerRespondTo
---
+++ resource_controller_respond_to.gemspec
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |s|
+  s.name = %q{resource_controller_respond_to}
+  s.version = "0.0.1"
+  
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.add_dependency('giraffesoft-resource_controller', '>= 0.6.5')
+  
+  s.authors = ["jesperronn", "Averell"]
+  s.date = %q{2008-11-08}
+  s.description = %q{Extends ResourceController plugin for Rails to make it respond_to .xml and .json formats.}
+  s.email = %q{mail@digineo.de}
+  s.files = %W{
+    ./tasks/resource_controller_respond_to_tasks.rake
+    ./init.rb
+    ./install.rb
+    ./Rakefile
+    ./lib/resource_controller_respond_to.rb
+    ./test/resource_controller_respond_to_test.rb
+    ./README
+    ./MIT-LICENSE
+    ./resource_controller_respond_to.gemspec
+    ./uninstall.rb
+  }
+  s.has_rdoc = true
+  s.homepage = %q{http://github.com/Averell/resource_controller_respond_to/tree/master}
+  s.rdoc_options = ["--inline-source", "--charset=UTF-8"]
+  s.require_paths = ["lib"]
+  s.rubygems_version = %q{1.3.0}
+  s.summary = %q{Extends ResourceController plugin for Rails to make it respond_to .xml and .json formats.}
+end
